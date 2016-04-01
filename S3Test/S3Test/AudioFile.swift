f3c410abed97cdf5767df9d757b989574d344d54
Sean Calkins <calkins.sean@gmail.com>
2016-04-01 12:15:58 -0600

    Still working on S3 saving

--- S3Test/S3Test/AudioFile.swift
+++ S3Test/S3Test/AudioFile.swift
@@ -12,10 +12,11 @@ import AmazonS3RequestManager
 
 class AudioFile {
     
-    let amazonS3Manager = AmazonS3RequestManager(bucket: "https://console.aws.amazon.com/s3/home?region=us-west-2&bucket=tiycourse&prefix=WatchOSAudio/",
+    let amazonS3Manager = AmazonS3RequestManager(bucket: "audiostorage-contentdelivery-mobilehub-1812299848",
                                                  region: .USStandard,
-                                                 accessKey: "AKIAJJRDLLRE5UHRFDJQ",
-                                                 secret: "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")
+                                                 accessKey: "AKIAIN7QEUNTRZONCL5A",
+                                                 secret: "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")
+
     
     let firebaseRef = Firebase(url: "https://watchosapp.firebaseio.com/audiofiles")
     var ref: Firebase?
